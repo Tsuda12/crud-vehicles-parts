1fb571988d43854a90a804271bd99fa6a9067724
Tsuda12 <felipe.tsuda01@gmail.com>
2024-09-28 15:10:09 -0300

    feat: add validation in create vehicle request

--- src/main/java/br/com/tsuda/backend/domain/dto/request/VehicleRequestDto.java
+++ src/main/java/br/com/tsuda/backend/domain/dto/request/VehicleRequestDto.java
@@ -1,8 +1,13 @@
 package br.com.tsuda.backend.domain.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record VehicleRequestDto(
+        @NotBlank(message = "Brand is required")
         String brand,
+        @NotBlank(message = "Model is required")
         String model,
+        @NotBlank(message = "Year of manufacture is required")
         String yearOfManufacture
 ) {
 }
