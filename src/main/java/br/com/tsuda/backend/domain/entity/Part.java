0f2a4978ab15ecea53ac283014f18ba19175d784
Felipe Tsuda <felipe.tsuda@pecaai.com.br>
2024-09-30 14:41:12 -0300

    feat: part entity

---
+++ src/main/java/br/com/tsuda/backend/domain/entity/Part.java
@@ -0,0 +1,67 @@
+package br.com.tsuda.backend.domain.entity;
+
+import br.com.tsuda.backend.controller.dto.response.VehicleResponseDto;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.List;
+
+@Entity
+@Table(name = "part")
+public class Part {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String partNumber;
+    private String brand;
+    private String model;
+    @OneToMany
+    private List<Vehicle> vehicles;
+
+    public Part() {}
+
+    public Part(String partNumber, String brand, String model, List<Vehicle> vehicles) {
+        this.partNumber = partNumber;
+        this.brand = brand;
+        this.model = model;
+        this.vehicles = vehicles;
+    }
+
+    public String getPartNumber() {
+        return partNumber;
+    }
+
+    public void setPartNumber(String partNumber) {
+        this.partNumber = partNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(List<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+}
